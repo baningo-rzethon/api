6cf9573b8e3876384443910fcc88d92a4d42f6b2
Gabriel Ślawski <gabriel@baningo.com>
2018-12-09 12:54:31 +0100

    master: map

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -20,4 +20,14 @@ class Pages extends Controller
     {
         $this->view('index');
     }
+
+    public function map()
+    {
+        echo '
+
+        <iframe width="100%" height="450" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/directions?origin=3+Maja+4,+35-069+Rzeszów,+Polska&destination=3+Maja+28,+35-025+Rzeszów,+Polska&mode=walking&key=AIzaSyAI4fQkvZHWLGT4APtAVRzDftXHtQQA2so" allowfullscreen></iframe>
+        
+        
+        ';
+    }
 }
